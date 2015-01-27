e81da6068e11e10d2441bf8268976dc0d750f233
jiezli <altitude9527@gmail.com>
2015-01-27 17:54:06 +0800

    improve

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Android-packaging
+
 Android 打包工具
+
+
 
--- pack.py
+++ pack.py
@@ -4,21 +4,19 @@ import zipfile
 import shutil
 import sys
 import os
-# channels = ['xiaomi', 'baidu', 'm360', 'm91']
-
-
 
 apk_path = sys.argv[1]
 out_path = sys.argv[2]
+
 if not os.path.exists(out_path):
-    os.mkdir(out_path)
+    os.makedirs(out_path)
 
 name = os.path.basename(apk_path)
 apk_name = name[:len(name) - 4] + "_{}.apk"
 
+channels_file = open('channels.txt')
 
-file = open('channels.txt')
-for channel in file:
+for channel in channels_file:
     channel_apk_name = (out_path + '/' + apk_name).format(channel.strip())
     shutil.copy2(apk_path, channel_apk_name)
     zipped = zipfile.ZipFile(channel_apk_name, 'a', zipfile.ZIP_DEFLATED)
